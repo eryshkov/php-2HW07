3ff4ac9bc5bd3a213a33e9fd8c0c3115b80e2106
Evgenii Ryshkov <eryshkov@gmail.com>
2019-04-07 13:39:46 +0300

    AdminDataTable.php is added

---
+++ App/AdminDataTable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use App\Models\Model;
+
+class AdminDataTable
+{
+    /**
+     * AdminDataTable constructor.
+     * @param Model[] $models
+     * @param callable[] $functions
+     */
+    public function __construct(array $models, array $functions)
+    {
+    }
+    
+    public function render()
+    {
+    
+    }
+}
