1ee8f1b15eb127e8707b86114de9afb31b231ee9
Evgenii Ryshkov <eryshkov@gmail.com>
2019-04-07 13:54:49 +0300

    AdminDataTable.php is fixed

--- App/AdminDataTable.php
+++ App/AdminDataTable.php
@@ -7,15 +7,27 @@ use App\Models\Model;
 class AdminDataTable
 {
     /**
+     * @var Model[] $models
+     */
+    protected $models;
+    
+    /**
+     * @var callable[] $functions
+     */
+    protected $functions;
+    
+    /**
      * AdminDataTable constructor.
      * @param Model[] $models
      * @param callable[] $functions
      */
     public function __construct(array $models, array $functions)
     {
+        $this->models = $models;
+        $this->functions = $functions;
     }
     
-    public function render()
+    public function render(): void
     {
     
     }
