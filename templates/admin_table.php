076f853fd40ed586b5342a851b90bb1d99bda157
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-17 13:32:34 +0300

    admin_table.php fixed

--- templates/admin_table.php
+++ templates/admin_table.php
@@ -10,11 +10,11 @@ while ($dataTable->valid()) {
     <div class="row mb-1">
         <div class="col-auto">
             <a class="btn btn-outline-info"
-               href="/admin/article/edit/<?php echo $dataTable->current(); ?>">✎</a>
+               href="/admin/article/<?php echo $dataTable->current(); ?>/edit">✎</a>
         </div>
         <div class="col-auto">
             <a class="btn btn-outline-danger"
-               href="/admin/article/delete/<?php echo $dataTable->current(); ?>">X</a>
+               href="/admin/article/<?php echo $dataTable->current(); ?>/delete">X</a>
         </div>
         <div class="col">
             <a href="/article/<?php echo $dataTable->current(); ?>">
