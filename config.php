1136088f09196ff64c7e7640bbd46dfd06e986e7
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-17 13:34:03 +0300

    config.php fixed

--- config.php
+++ config.php
@@ -10,6 +10,16 @@ $result = [
         'password' => '123',
     ],
     'log' => __DIR__ . '/log.txt',
+    'routes' => [
+        '/article/{id}'              => \App\Controllers\Article::class,
+        '/admin/article/add'         => \App\Controllers\Admin\Article\InsertPresenter::class,
+        '/admin/article/{id}/edit'   => \App\Controllers\Admin\Article\EditPresenter::class,
+        '/admin/article/{id}/delete' => \App\Controllers\Admin\Article\Delete::class,
+        '/admin/article/update'      => \App\Controllers\Admin\Article\Update::class,
+        '/admin/article/create'      => \App\Controllers\Admin\Article\Create::class,
+        '/admin'                     => \App\Controllers\Admin::class,
+        ''                           => \App\Controllers\Index::class,
+    ],
 ];
 
 return $result;
